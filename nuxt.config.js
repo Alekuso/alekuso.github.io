ca40ded11fd1805b206f5a8935e9980967404c87
Alexou <alexouoffi@gmail.com>
2021-07-19 03:15:35 +0200

    inter font

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,8 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' }
     ]
   },
 
